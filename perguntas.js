8598a94ffcb275bc64483ebd7b940c108e9fb5ed
QUADROS08 <quadros.enzo@escola.pr.gov.br>
2025-05-12 14:46:43 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Programacao',
+    'O que JavaScript?',
+    'Linguagem de Programação',
+)
+
+criaCartao(
+    'Ingles'
+    'Como se escreve dia do Pagamento?' 
+    'Pay Day'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual o gráfico fa função quadrádica',
+    'Parabolas'
+)
+
